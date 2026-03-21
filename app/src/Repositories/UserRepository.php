5bed62cd61756598aedd512d9835803023b80a1c
Mathis <vidueiramathis@gmail.com>
2026-03-21 01:20:56 +0100

    feat: authentification add login endpoint with jwt token issuance

---
+++ app/src/Repositories/UserRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Entities\User;
+use App\Lib\Repositories\AbstractRepository;
+
+class UserRepository extends AbstractRepository {
+    public function getTable(): string {
+        return 'users';
+    }
+
+    public function findByEmail(string $email): ?User {
+        $query = 'SELECT * FROM users WHERE email = :email LIMIT 1';
+        $statement = $this->db->getConnexion()->prepare($query);
+        $statement->execute(['email' => $email]);
+        $statement->setFetchMode(\PDO::FETCH_CLASS, User::class);
+
+        $user = $statement->fetch();
+
+        return $user === false ? null : $user;
+    }
+}
+
+?>
